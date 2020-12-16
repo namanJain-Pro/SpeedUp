e5436e625b566e2e59c077ad54a7d51fef01cf27
Naman Jain <worknj15@gmail.com>
2020-12-16 18:26:36 +0530

    main gui some progress

---
+++ src/Scene/MainScreen/MainController.java
@@ -0,0 +1,4 @@
+package Scene.MainScreen;
+
+public class MainController {
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module Speed.Up.Mini.Project.With.Planning {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens Run;
+    opens Scene.MainScreen;
+}
